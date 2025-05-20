0798f7d195affbc9883f4e990310a2ae99bf9e96
Lucas Esteves <esteveslucasreis@gmail.com>
2025-05-19 23:54:37 -0300

    adding first alembic migration, creating users table.

--- tests/test_app.py
+++ tests/test_app.py
@@ -71,8 +71,46 @@ def test_update_user_should_return_ok_and_user(client):
     }
 
 
+def test_update_user_should_return_not_found(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'testuser',
+            'email': 'test@user.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_delete_user_should_return_ok_and_message(client):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted successfully'}
+
+
+def test_delete_user_should_return_not_found(client):
+    response = client.delete('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_user_should_return_ok_and_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'testuser',
+        'email': 'test@user.com',
+        'id': 1,
+    }
+
+
+def test_get_user_should_return_not_found(client):
+    response = client.get('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
